ef59d0cd9908dd99692e87a15c9aa91b7f10fc5c
ritaamritkar1212 <rita.amritkar@gmail.com>
2023-01-26 11:55:48 +0400

    facebook and twitter strategy added

--- lib/omniauth/social.rb
+++ lib/omniauth/social.rb
@@ -1,3 +1,5 @@
 require "omniauth/social/version"
 require 'omniauth/strategies/tiktok'
 require 'omniauth/strategies/google_oauth2'
+require 'omniauth/strategies/twitter'
+require 'omniauth/facebook'
